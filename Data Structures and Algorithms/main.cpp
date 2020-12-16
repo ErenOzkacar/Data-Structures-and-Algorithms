944bad3856dec9b26fc4a8f33ccac9c806f6f13a
ErenOzkacar <ErenOzkacar@icloud.com>
2020-12-16 02:07:22 +0100

    Initial Commit

---
+++ Data Structures and Algorithms/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Data Structures and Algorithms
+//
+//  Created by Eren Özkacar on 16.12.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
